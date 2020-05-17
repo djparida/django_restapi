fa9cd1fadaa1b5ba5df0da148e64ff83ee419b84
Dibyajyoti Parida <dibyajyoti@centroxy.com>
2020-05-17 11:38:09 +0530

    rest api

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\centroxy\Python\Django-REST-Framework-User-Registration-Authentication\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
